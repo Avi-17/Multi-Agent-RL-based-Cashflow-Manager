ba256b7e39fdcfc350939abba3439fe8a15746a1
Bhavishya <sahaybhavishya@gmail.com>
2026-04-25 18:18:04 +0530

    Changed port mapping

--- server/app.py
+++ server/app.py
@@ -388,7 +388,7 @@ app = gr.mount_gradio_app(app, gradio_app, path="/ui")
 
 def main():
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7861)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
